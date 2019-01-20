69bd579eac06aad6bcaa96c7fbd40c7663d23d00
iambalajive <balajivenkiteswaran@gmail.com>
2019-01-21 00:50:16 +0530

    Add cors support

--- src/main/scala/com/cars/adverts/app/Application.scala
+++ src/main/scala/com/cars/adverts/app/Application.scala
@@ -6,6 +6,9 @@ import com.cars.adverts.migration.DBMigrator
 import io.dropwizard.setup.{Bootstrap, Environment}
 import ru.vyarus.dropwizard.guice.GuiceBundle
 import com.datasift.dropwizard.scala.ScalaApplication
+import org.eclipse.jetty.servlets.CrossOriginFilter
+import javax.servlet.DispatcherType
+import java.util
 
 
 object Application extends ScalaApplication[AppConfiguration] {
@@ -15,7 +18,20 @@ object Application extends ScalaApplication[AppConfiguration] {
   }
 
   override def run(conf: AppConfiguration, env: Environment): Unit = {
-//    new DBMigrator(conf.database.jdbcUrl,conf.database.username,conf.database.password).migrate
+
+    new DBMigrator(conf.database.jdbcUrl,conf.database.username,conf.database.password).migrate
+
+    val cors = env.servlets().addFilter("CORS", classOf[CrossOriginFilter])
+
+    //Generic CORS setup . there is auth required in the app
+    // Configure CORS parameters
+    cors.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*")
+    cors.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "X-Requested-With,Content-Type,Accept,Origin,Authorization")
+    cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "OPTIONS,GET,PUT,POST,DELETE,HEAD")
+    cors.setInitParameter(CrossOriginFilter.ALLOW_CREDENTIALS_PARAM, "true")
+
+    // Add URL mapping
+    cors.addMappingForUrlPatterns(util.EnumSet.allOf(classOf[DispatcherType]), true, "/*")
   }
 }
 
